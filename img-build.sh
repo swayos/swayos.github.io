2dc13d1598d59d1414b6e7748991eb56829ed28a
Milan Toth <milgramilgra@gmail.com>
2022-04-22 17:01:13 +0200

    updates

--- img-build.sh
+++ img-build.sh
@@ -12,13 +12,14 @@ exec 1> >(tee "tmp/img_build_out")
 exec 2> >(tee "tmp/img_build_err")
 
 # Create disk image for package install
-dd if=/dev/zero of=swayos.img bs=1M count=6000
+dd if=/dev/zero of=swayos.img bs=1M count=5500
 mkfs.ext4 -F swayos.img
 
 # Pacstrap all needed packages
-sudo mount swayos.img /mnt
-cat pacs/base pacs/swayos pacs/aurdeps > tmp/pacs
-sudo pacstrap /mnt - < tmp/pacs
+sudo mkdir -p /mnt/swayos
+sudo mount swayos.img /mnt/swayos
+cat pacs/base pacs/swayos > tmp/pacs
+sudo pacstrap /mnt/swayos - < tmp/pacs
 
 # Precompile, copy and install aur packages to image
 cd tmp
@@ -31,26 +32,30 @@ do
     makepkg -s --skippgpcheck
     cd ..
     # Move to target image
-    sudo cp -r $package /mnt/home/
+    sudo cp -r $package /mnt/swayos/home/
     # Install on target image
     rel_path=$(ls $package/*.pkg.tar.zst)
-    sudo arch-chroot /mnt pacman --noconfirm -U "/home/$rel_path"
+    sudo arch-chroot /mnt/swayos pacman --noconfirm -U "/home/$rel_path"
     # cleanup
-    sudo rm -r /mnt/home/$package
+    sudo rm -r /mnt/swayos/home/$package
 done
 cd ..
 
 # Copy fonts
-sudo cp -r font/*.* /mnt/usr/share/fonts/TTF/
+sudo cp -r font/*.* /mnt/swayos/usr/share/fonts/TTF/
 
 # Chown font files
-sudo arch-chroot /mnt chown --recursive "root:root" /usr/share/fonts/TTF
+# sudo arch-chroot /mnt/swayos chown --recursive "root:root" /usr/share/fonts/TTF
 
 # Copy config files
-sudo cp -f -R home /mnt/home/configs
+sudo cp -f -R home /mnt/swayos/home/configs
 
 # Unmount
-sudo umount /mnt
+sudo umount /mnt/swayos
+
+# shrink
+e2fsck -f -y swayos.img
+resize2fs -M swayos.img
 
 # Zip
 gzip swayos.img
