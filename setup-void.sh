47f212ca9baec07660d15c7985b092d08f14009a
Milan Toth <milgramilgra@gmail.com>
2022-12-25 20:56:41 +0100

    void setup change

--- setup-void.sh
+++ setup-void.sh
@@ -22,57 +22,232 @@ check(){
     fi
 }
 
-log "Refreshing package db"
+
+log "Disable beeping"
+
+echo 'rmmod pcspkr' | sudo tee -a /etc/modprobe.d/blacklist.conf
+
+
+#log "Force unblock"
+#echo 'rfkill unblock all' | sudo tee -a /etc/rc.local
+
+
+log "Disable grub menu"
+
+echo 'GRUB_TIMEOUT_STYLE=hidden' | sudo tee -a /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="loglevel=1 quiet splash"' | sudo tee -a /etc/default/grub
+
+
+log "Enable shutdown/reboot/suspend"
+
+echo 'GRUB_TIMEOUT_STYLE=hidden' | sudo tee -a /etc/default/grub
+
+
+log "Update xbps"
+
 sudo xbps-install -u xbps
+
+
+log "System update"
+
 sudo xbps-install -Su
-check "$?" "package update"
 
 
-log "Installing git"
-sudo xbps-install --yes git
+log "Install base tools"
+
+sudo xbps-install \
+ bc \
+ zsh \
+ zsh-autosuggestions \
+
+ 
+log "Install Sway environment"
+
+sudo xbps-install \
+ Adapta \
+ Waybar \
+ blueman \
+ bluez \
+ brightnessctl \
+ cups \
+ emacs \
+ foot \
+ grim \
+ grub \
+ iwd \
+ libreoffice \
+ libspa-bluetooth \
+ mesa-dri \
+ nautilus \
+ pamixer \
+ pavucontrol \
+ pipewire \
+ seatd \
+ slurp \
+ sway \
+ swaybg \
+ swayidle \
+ swaylock \
+ system-config-printer \
+ terminus-font \
+ wireplumber \
+ wl-clipboard \
+ wofi \
+ xdg-desktop-portal-wlr \
+ wob \
+ iwgtk
+
+
+log "Enable services"
+
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/iwd /var/service
+sudo ln -s /etc/sv/seatd /var/service
+sudo ln -s /etc/sv/bluetoothd /var/service
+sudo ln -s /etc/sv/pipewire /var/service
+sudo ln -s /etc/sv/pipewire-pulse /var/service
+
+
+log "Disable wpa_supplicant"
+
+sudo rm /var/service/wpa_supplicant
+
+
+log "Disable iwd udevd collosion"
+
+echo '[General]' | sudo tee -a /etc/iwd/main.conf
+echo 'UseDefaultInterface=true' | sudo tee -a /etc/iwd/main.conf
+
+
+log "Add user to groups"
+
+sudo usermod -a $USER -G _seatd
+sudo usermod -a $USER -G bluetooth
+
+
+log "Changin shell to zsh"
+
+chsh -s /bin/zsh
+
+
+log "Installing Google Chrome"
+
+git clone git://github.com/void-linux/void-packages.git && cd void-packages/ && ./xbps-src binary-bootstrap && echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf && ./xbps-src pkg google-chrome
+
+sudo xbps-install google-chrome --repository=hostdir/binpkgs/nonfree
+
+
+log "Installing dev tools"
+
+sudo xbps-install \
+ git \
+ pkg-config \
+ meson \
+ wayland-protocols
+
+
+log "Installing latest FFMPEG"
+
+curl https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.xz --output ffmpeg-5.1.2.tar.xz
+tar -xvf ffmpeg-5.1.2.tar.xz
+cd ffmpeg
+./configure \
+  --prefix=/usr \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-libs=-lpthread \
+  --extra-libs=-lm \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-libfdk_aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree \
+  --enable-shared
+make
+sudo make install
+cd ..
+
+
+log "Updating library cache"
+sudo ldconfig
+
+
+log "Install sov"
+
+git clone https://github.com/milgra/sov
+sudo xbps-install meson ninja pkg-config clang libpng-devel freetype-devel libglvnd-devel glew-devel wayland-devel libxkbcommon-devel
+cd sov
+meson setup build --buildtype=releaase
+ninja -C build
+sudo ninja -C build install
+cd ..
+
+
+log "Install wcp"
+
+git clone https://github.com/milgra/wcp
+cd wcp
+meson setup build --buildtype=releaase
+ninja -C build
+sudo ninja -C build install
+cd ..
+
+
+log "Install vmp"
+
+git clone https://github.com/milgra/vmp
+sudo xbps-install SDL2-devel ffmpeg-devel jpeg-devel jbig2dec-devel openjp2-devel libopenjpeg2-devel harfbuzz-devel
+cd vmp
+meson setup build --buildtype=releaase
+ninja -C build
+sudo ninja -C build install
+cd ..
+
+
+log "Install mmfm"
+
+git clone https://github.com/milgra/mmfm
+sudo xbps-install mupdf-devel gumbo-parser-devel mujs-devel
+cd mmfm
+meson setup build --buildtype=releaase
+ninja -C build
+sudo ninja -C build install
+cd ..
 
 
 log "Cloning swayOS repo"
+
 git clone https://github.com/swayos/swayos.github.io.git
 cd swayos.github.io
 
 
-log "Installing needed official packages"
-for line in $(cat pacs/online); do sudo xbps-install "$line"; done
-check "$?" "package download"
-
-
 log "Copying terminus-ttf fonts to font directory"
+
 sudo cp -f font/*.* /etc/fonts/
 check "$?" "cp"
 
 
 log "Copying settings to home folder"
+
 cp -f -R home/. ~/
 check "$?" "cp"
 
 
-log "Adding user to needed groups"
-usermod -a -G bluetooth milgra
-usermod -a -G _seatd milgra
-
-
-log "Starting services"
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/seatd /var/service
-sudo ln -s /etc/sv/iwd /var/service
-sudo ln -s /etc/sv/bluetoothd /var/service
-
-
 log "Cleaning up"
+
 cd ..
 rm -f -R swayos.github.io
 check "$?" "rm"
 
-
-log "Changing shell to zsh"
-chsh -s /bin/zsh
-check "$?" "chsh"
-
+# dconf-editor?
+# remove ffmpeg src?
+# remove sov, wcp, vmp, mmfm src?
+# remove chrome src
 
 log "Setup is done, please log out and log in back again ( type exit )"
