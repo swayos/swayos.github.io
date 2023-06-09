4d00bfb4c14f2e2375b44a2f6b56e0990c49c2d4
Milan Toth <milgramilgra@gmail.com>
2023-06-09 22:45:12 +0200

    pipewire update for void linux

--- home/.pipewire.sh
+++ home/.pipewire.sh
@@ -1,4 +1,9 @@
 # dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway &
+pipewire &
+pipewire-pulse &
+sleep 2
+wireplumber &
+sleep 2
 /usr/libexec/xdg-desktop-portal-wlr -r &
-sleep 5
+sleep 2
 /usr/libexec/xdg-desktop-portal -r &
