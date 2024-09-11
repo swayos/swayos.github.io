1445be9db0a1f84f43bca1d80aae05e4956b27ec
Milan Toth <milgramilgra@gmail.com>
2024-09-11 12:09:55 +0200

    fix status.sh

--- home/.config/sway/status.sh
+++ home/.config/sway/status.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # The Sway configuration file in ~/.config/sway/config calls this script.
 # You should see changes to the status bar after saving this script.
 # If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
